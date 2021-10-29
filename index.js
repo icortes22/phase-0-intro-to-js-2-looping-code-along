2b0832a77860d7f5d0636114b7e76c260e8af037
Isamar <isamarcortes589@gmail.com>
2021-10-29 16:56:46 -0400

    commit tree 6946c7e134d3f970a2018afba258c49283d0f765

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, feeling){
+    let emptyCards = []
+    for(let i = 0; i < array.length; i++){
+        emptyCards.push(`Thank you, ${array[i]}, for the wonderful ${feeling} gift!`)
+    }
+    return emptyCards
+}
+function countDown(number){
+    while (number > 0){
+        console.log(number)
+        number -= 1 
+    }
+    console.log(number)
+}
